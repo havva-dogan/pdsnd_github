99f6e0e95e39cbdda2d5e36635d2e0c6d44379ae
Havva_Dogan <hawadogan@gmail.com>
2021-05-30 13:34:38 +0300

    added doc and expl.

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,9 @@ import time
 import pandas as pd
 import numpy as np
 
+"""
+Dictionary for city csv files
+"""
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
